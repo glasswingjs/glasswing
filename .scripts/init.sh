4924806e81568e692a61a00bed8a41425b7107a3
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2019-12-10 00:10:27 +0200

    this should serve as an init script

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-PROJECTS="application common config controller http router template"
-
-mkdir -p "./node_modules/@glasswing"
-
-git clone https://github.com/glasswingjs/glasswing ./node_modules/glasswing
+PROJECTS="template common config http router controller application"
 
 for project in $PROJECTS; do
-  PROJECT_FOLDER="./node_modules/@glasswing/$project"
+  PROJECT_FOLDER="./$project"
   [ -d $PROJECT_FOLDER ] || git clone https://github.com/glasswingjs/$project $PROJECT_FOLDER
-  [ -d $PROJECT_FOLDER ] && cd $PROJECT_FOLDER && git pull && npm i && rm -rf node_modules/@glasswing && cd ../../..
+  [ -d $PROJECT_FOLDER ] \
+    && cd $PROJECT_FOLDER \
+    && git pull \
+    && npm i \
+    && npm run build \
+    && ( npm run prepare-for-dev || true )  \
+    && cd ..
 done
